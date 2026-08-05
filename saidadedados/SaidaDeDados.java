0424a8b907e8e33b7e138bb02251b52f98681521
Taiana dos Santos <136939790+naeorii@users.noreply.github.com>
2026-08-05 20:09:05 -0300

    Upload dos exemplos feitos em aula

---
+++ saidadedados/SaidaDeDados.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package saidadedados;
+
+/**
+ *
+ * @author Aluno
+ */
+public class SaidaDeDados {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int idade = 25;
+        
+        //println: imprime e quebra a linha
+        System.out.println("Idade: " + idade);
+        
+        //print: sem quebra de linha
+        System.out.print("Java, ");
+        
+        //printf: texto formatado
+        System.out.printf("%d anos. \n", idade);
+    }
+    
+}
